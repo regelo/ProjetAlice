0fbb3c1cb2ce7e4e79ba8cccde25b2c57364c56c
Bob <bob@bob.com>
2020-03-04 08:26:05 -0500

    Modifications dans le main

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+﻿// ConsoleApplication1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
 #include <iostream>
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "TEST #1";
+    std::cout << "TEST #2";
+alksjdlkajaljlkjlkajlkssajld
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
