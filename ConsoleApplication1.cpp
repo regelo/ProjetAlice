21e998f6e79791d4f4bec20e30bac11eabb0b529
regelo <34382451+regelo@users.noreply.github.com>
2020-04-09 09:29:17 -0400

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,18 +5,7 @@
 
 int main()
 {
-    std::cout << "TEST #1";
-    std::cout << "TEST #2";
-    std::cout << "Test #3";
+    std::cout << "TEST #1" << std::endl;
+    std::cout << "TEST #2" << std::endl;
+    std::cout << "Test #3" << std::endl;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
